a5383ff575dfefee0e8c9c632d8eed557ac5675e
Zainab Junaid <153438938+Zainab-Junaid@users.noreply.github.com>
2025-02-15 21:27:22 +0500

    Update Group Anagrams.py

--- Group Anagrams.py
+++ Group Anagrams.py
@@ -1,1 +1,11 @@
-
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        anagram = {}
+        for elem in strs:
+            s = ''.join(sorted(elem))
+            if s in anagram:
+                anagram[s].append(elem)
+            else:
+                anagram[s] = [elem]
+        return list(anagram.values())
+        
