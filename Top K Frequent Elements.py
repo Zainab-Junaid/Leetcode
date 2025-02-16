d9bb59b4e6b5b173806a034bde08a5d1618f09c7
Zainab Junaid <153438938+Zainab-Junaid@users.noreply.github.com>
2025-02-16 12:42:39 +0500

    Update Top K Frequent Elements.py

--- Top K Frequent Elements.py
+++ Top K Frequent Elements.py
@@ -1,1 +1,14 @@
-
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        d_count = {}
+        l = []
+        for elem in nums:
+            if elem in d_count:
+                d_count[elem] += 1
+            else:
+                d_count[elem] = 1
+        d = sorted(d_count.items(), key = lambda x: x[1], reverse = True)
+        for i in range(k):
+            l.append(d[i][0])
+            print()
+        return l
